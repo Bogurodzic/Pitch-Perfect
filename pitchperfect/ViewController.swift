d91f98542ebaf132e7fe81b834953e139eb3c921
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-24 17:06:36 +0200

    Added record button, stoprecord button, label and assets

--- pitchperfect/ViewController.swift
+++ pitchperfect/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false;
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record the button was pressed")
+        recordingLabel.text = "Recording in progres..."
+    }
+    
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop the record button was pressed")
+    }
+    
+    
+    
 }
 
